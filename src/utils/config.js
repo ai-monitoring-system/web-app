03c39e7d5eea53e40a0f0a513e6cea1f7c3f87dd
Yousef <karty@uwindsor.ca>
2024-12-01 01:30:17 -0500

    changed db key

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,12 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDITg40yAhlbAfnqvNdnons7NlmUYwIasQ",
-  authDomain: "project-1d564.firebaseapp.com",
-  projectId: "project-1d564",
-  storageBucket: "project-1d564.firebasestorage.app",
-  messagingSenderId: "762574644488",
-  appId: "1:762574644488:web:86ee4fe8b62a17386bf8bd"
+  apiKey: "AIzaSyDW-vTVTDaoaAzCgVCegs2d_JUyaj-g7Js",
+  authDomain: "apms-python-server.firebaseapp.com",
+  projectId: "apms-python-server",
+  storageBucket: "apms-python-server.firebasestorage.app",
+  messagingSenderId: "104880636657",
+  appId: "1:104880636657:web:b1e4c0524d076c6fbd5f50",
+  measurementId: "G-V69FMTBMNG",
 };
 
 const app = initializeApp(firebaseConfig);
